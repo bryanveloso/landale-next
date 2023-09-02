2d84351e3bd9cbc83ff908cc6af13512dfd5e21c
Bryan Veloso <bryan@velo.so>
2023-09-01 22:17:32 -0700

    Stop the background from creeping.

--- src/pages/overlays/cameras/horizontal.tsx
+++ src/pages/overlays/cameras/horizontal.tsx
@@ -5,13 +5,13 @@ import { SmallCloseIcon, SmallFullScreenIcon } from '@/components/icons'
  */
 const HorizontalCamera = () => {
   return (
-    <div className="flex flex-col w-[426px] m-6 bg-[#343434] shadow-black/50 shadow-xl rounded-xl ring-1 ring-offset-0 ring-inset ring-white/20">
-      <div className="flex-auto flex justify-center items-center gap-2 p-2 px-4 text-sm text-white/25 font-bold">
-        <SmallCloseIcon className="w-3 h-3" />
-        <SmallFullScreenIcon className="w-3 h-3" />
-        <div className="flex-auto text-center">test</div>
+    <div className={`flex flex-col w-[426px] m-6 bg-[#343434] shadow-black/50 shadow-xl rounded-xl ring-1 ring-offset-0 ring-inset ring-white/20`}>
+      <div className={`flex-auto flex justify-center items-center gap-2 p-2 px-4 text-sm text-white/25 font-bold`}>
+        <SmallCloseIcon className={`w-3 h-3`} />
+        <SmallFullScreenIcon className={`w-3 h-3`} />
+        <div className={`flex-auto text-center`}>&nbsp;</div>
       </div>
-      <div className="aspect-video m-2 mt-0 bg-[#1E1E1E] rounded-lg"></div>
+      <div className={`aspect-video m-2 mt-0 bg-[#1E1E1E] rounded-lg`}></div>
     </div>
   )
 }
